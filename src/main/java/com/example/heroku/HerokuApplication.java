5948c85ce09165586c49c8c8b5ddb255cc5d4adf
Balu <balakrishna.gujju@outlook.com>
2020-01-14 16:33:42 +0530

    Update HerokuApplication.java

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -74,7 +74,88 @@ public class HerokuApplication {
       return "error";
     }
   }
+ @RequestMapping("/TICKAPI")
+  String db(Map<String, Object> model) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
+      ArrayList<String> output = new ArrayList<String>();
+      while (rs.next()) {
+        output.add("Read from DB: " + rs.getTimestamp("tick"));
+      }
+
+      model.put("records", convert(rs));
+      return "db";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
+public static JSONArray convert( ResultSet rs )
+    throws SQLException, JSONException
+  {
+    JSONArray json = new JSONArray();
+    ResultSetMetaData rsmd = rs.getMetaData();
+
+    while(rs.next()) {
+      int numColumns = rsmd.getColumnCount();
+      JSONObject obj = new JSONObject();
+
+      for (int i=1; i<numColumns+1; i++) {
+        String column_name = rsmd.getColumnName(i);
+
+        if(rsmd.getColumnType(i)==java.sql.Types.ARRAY){
+         obj.put(column_name, rs.getArray(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.BIGINT){
+         obj.put(column_name, rs.getInt(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.BOOLEAN){
+         obj.put(column_name, rs.getBoolean(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.BLOB){
+         obj.put(column_name, rs.getBlob(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.DOUBLE){
+         obj.put(column_name, rs.getDouble(column_name)); 
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.FLOAT){
+         obj.put(column_name, rs.getFloat(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.INTEGER){
+         obj.put(column_name, rs.getInt(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.NVARCHAR){
+         obj.put(column_name, rs.getNString(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.VARCHAR){
+         obj.put(column_name, rs.getString(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.TINYINT){
+         obj.put(column_name, rs.getInt(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.SMALLINT){
+         obj.put(column_name, rs.getInt(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.DATE){
+         obj.put(column_name, rs.getDate(column_name));
+        }
+        else if(rsmd.getColumnType(i)==java.sql.Types.TIMESTAMP){
+        obj.put(column_name, rs.getTimestamp(column_name));   
+        }
+        else{
+         obj.put(column_name, rs.getObject(column_name));
+        }
+      }
+
+      json.put(obj);
+    }
+
+    return json;
+  }
   @Bean
   public DataSource dataSource() throws SQLException {
     if (dbUrl == null || dbUrl.isEmpty()) {
